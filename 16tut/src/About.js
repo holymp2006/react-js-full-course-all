9c0249319c1c68f9181162598572e28866fb945b
Sam <holymp2006@gmail.com>
2022-12-28 23:55:11 +0300

    feat: Add Router Hooks & Links

---
+++ 16tut/src/About.js
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <main className="About">
+      <h2>About</h2>
+      <p style={{ marginTop: "1rem" }}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus,
+        laudantium. Repellendus saepe mollitia ab reprehenderit illo officiis
+        autem odit dolores molestiae at, nihil laborum praesentium iste ducimus
+        itaque earum impedit.
+      </p>
+    </main>
+  );
+};
+
+export default About;
