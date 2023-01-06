19539efd6df284364775984a9f9506f6483a3ad9
Sam <holymp2006@gmail.com>
2023-01-06 21:11:15 +0300

    fix: Remove unused imports /10tut

--- 10tut/src/App.js
+++ 10tut/src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import styles from "./App.module.scss";
 import ColorSquare from "./ColorSquare";
 import SelectColorInput from "./SelectColorInput";
